9c44efbee07e237f7d222f20ebc0b0295dbc1387
James Kirkby <jkirkby@protonmail.ch>
2017-06-28 01:52:17 +0800

    remove access to user

--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -1,123 +1,133 @@
 <?php
 
-namespace Jkirkby91\LumenRestServerComponent\Http\Controllers;
+	namespace Jkirkby91\LumenRestServerComponent\Http\Controllers;
 
-use Psr\Http\Message\ServerRequestInterface;
-use Spatie\Fractal\ArraySerializer AS ArraySerialization;
-use Jkirkby91\Boilers\RestServerBoiler\ResourceControllerContract;
-use Jkirkby91\LumenRestServerComponent\Libraries\ResourceResponseTrait;
-use Jkirkby91\LumenRestServerComponent\Http\Controllers\RestController;
-use Jkirkby91\Boilers\RestServerBoiler\Exceptions\NotFoundHttpException;
-use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnauthorizedHttpException;
-use Jkirkby91\Boilers\RestServerBoiler\TransformerContract AS ObjectTransformer;
-use Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract AS ResourceRepository;
+	use Psr\Http\Message\ServerRequestInterface;
+	use Spatie\Fractal\ArraySerializer AS ArraySerialization;
+	use Jkirkby91\Boilers\RestServerBoiler\ResourceControllerContract;
+	use Jkirkby91\LumenRestServerComponent\Libraries\ResourceResponseTrait;
+	use Jkirkby91\LumenRestServerComponent\Http\Controllers\RestController;
+	use Jkirkby91\Boilers\RestServerBoiler\Exceptions\NotFoundHttpException;
+	use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnauthorizedHttpException;
+	use Jkirkby91\Boilers\RestServerBoiler\TransformerContract AS ObjectTransformer;
+	use Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract AS ResourceRepository;
 
-/**
- * Class ResourceController
- *
- * @package Jkirkby91\LumenRestServerComponent\Http\Controllers
- * @author James Kirkby <jkirkby91@gmail.com>
- */
-abstract class ResourceController extends RestController implements ResourceControllerContract
-{
+	/**
+	 * Class ResourceController
+	 *
+	 * @package Jkirkby91\LumenRestServerComponent\Http\Controllers
+	 * @author  James Kirkby <jkirkby@protonmail.ch>
+	 */
+	abstract class ResourceController extends RestController implements ResourceControllerContract
+	{
 
-    use ResourceResponseTrait;
+		use ResourceResponseTrait;
 
-    /**
-     * @var ResourceRepository
-     */
-    protected $repository;
+		/**
+		 * @var \Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract
+		 */
+		protected $repository;
 
-    /**
-     * @var ObjectTransformer
-     */
-    protected $transformer;
+		/**
+		 * @var \Jkirkby91\Boilers\RestServerBoiler\TransformerContract
+		 */
+		protected $transformer;
 
-    /**
-     * RestController constructor.
-     *
-     * @param ResourceRepository $repository
-     * @param ObjectTransformer $objectTransformer
-     */
-    public function __construct(ResourceRepository $repository, ObjectTransformer $objectTransformer)
-    {
-        parent::__construct();
-        $this->repository   = $repository;
-        $this->transformer  = $objectTransformer;
-    }
+		/**
+		 * ResourceController constructor.
+		 *
+		 * @param \Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract $repository
+		 * @param \Jkirkby91\Boilers\RestServerBoiler\TransformerContract        $objectTransformer
+		 */
+		public function __construct(ResourceRepository $repository, ObjectTransformer $objectTransformer)
+		{
+			parent::__construct();
+			$this->repository   = $repository;
+			$this->transformer  = $objectTransformer;
+		}
 
-    /**
-     * @return mixed
-     */
-    public function index(ServerRequestInterface $request)
-    {
-        $page = $this->getPaginationPageFromRequest($request);
+		/**
+		 * index()
+		 * @param \Psr\Http\Message\ServerRequestInterface $request
+		 *
+		 * @return \Zend\Diactoros\Response\JsonResponse
+		 */
+		public function index(ServerRequestInterface $request)
+		{
+			$page = $this->getPaginationPageFromRequest($request);
 
-        try {
-            //@TODO implement softdelete and search on criteria that is still active
-            $results = $this->repository->findBy([],['id'=>'desc']);
-        } catch (ORMInvalidArgumentException $e){
-            $this->clientErrorResponse('Invalid Search Criteria');
-        } catch (ORMException $e){
-            return $this->serverErrorResponse();
-        }
-        
-        try {
-            $paginatedResults = $this->paginateResults($results,$page);
-        } catch (UnprocessableEntityException $e) {
-            return $this->serverErrorResponse();
-        } catch (NotFoundHttpException $e) {
-            return $this->notFoundResponse();
-        }
+			try {
+				//@TODO implement softdelete and search on criteria that is still active
+				$results = $this->repository->findBy([],['id'=>'desc']);
+			} catch (ORMInvalidArgumentException $e){
+				$this->clientErrorResponse('Invalid Search Criteria');
+			} catch (ORMException $e){
+				return $this->serverErrorResponse();
+			}
 
-        return $this->showResponse($paginatedResults);
-    }
+			try {
+				$paginatedResults = $this->paginateResults($results,$page);
+			} catch (UnprocessableEntityException $e) {
+				return $this->serverErrorResponse();
+			} catch (NotFoundHttpException $e) {
+				return $this->notFoundResponse();
+			}
 
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function show($id)
-    {
-        if($data = $this->repository->find($id))
-        {
-            $entity = $this->item($data)
-                        ->transformWith($this->transformer)
-                        ->serializeWith(new ArraySerialization());
-        } else {
-            throw new NotFoundHttpException();
-        }
+			return $this->showResponse($paginatedResults);
+		}
 
-        return $this->showResponse($entity);
-    }
+		/**
+		 * show()
+		 * @param $id
+		 *
+		 * @return \Zend\Diactoros\Response\JsonResponse
+		 */
+		public function show($id)
+		{
+			if ($data = $this->repository->find($id))
+			{
+				$entity = $this->item($data)
+					->transformWith($this->transformer)
+					->serializeWith($this->serializer);
+			} else {
+				throw new NotFoundHttpException();
+			}
 
-    /**
-     * @param ServerRequestInterface $request
-     * @return mixed
-     */
-    abstract public function store(ServerRequestInterface $request);
+			return $this->showResponse($entity);
+		}
 
-    /**
-     * @param ServerRequestInterface $request
-     * @param $id
-     * @return mixed|void
-     * @throws \Exception
-     */
-    abstract public function update(ServerRequestInterface $request, $id);
+		/**
+		 * store()
+		 * @param \Psr\Http\Message\ServerRequestInterface $request
+		 *
+		 * @return mixed
+		 */
+		abstract public function store(ServerRequestInterface $request);
 
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function destroy($id)
-    {
-        if(!$data = $this->repository->find($id))
-        {
-            throw new NotFoundHttpException;
-        }
+		/**
+		 * update()
+		 * @param \Psr\Http\Message\ServerRequestInterface $request
+		 * @param                                          $id
+		 *
+		 * @return mixed
+		 */
+		abstract public function update(ServerRequestInterface $request, $id);
 
-        $this->repository->destory($id);
+		/**
+		 * destroy()
+		 * @param $id
+		 *
+		 * @return \Zend\Diactoros\Response\JsonResponse
+		 */
+		public function destroy($id)
+		{
+			if(!$data = $this->repository->find($id))
+			{
+				throw new NotFoundHttpException;
+			}
 
-        return $this->deletedResponse();
-    }
-}
+			$this->repository->destory($id);
+
+			return $this->deletedResponse();
+		}
+	}
--- src/Http/Controllers/RestController.php
+++ src/Http/Controllers/RestController.php
@@ -1,139 +1,142 @@
 <?php
 
-namespace Jkirkby91\LumenRestServerComponent\Http\Controllers;
+	namespace Jkirkby91\LumenRestServerComponent\Http\Controllers;
 
-use Illuminate\Routing\Controller;
-use Psr\Http\Message\ServerRequestInterface;
-use Spatie\Fractal\ArraySerializer as ArraySerialization;
-use Jkirkby91\Boilers\RestServerBoiler\ResourceResponseContract;
-use Jkirkby91\Boilers\RestServerBoiler\Exceptions\NotFoundHttpException;
-use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnauthorizedHttpException;
-use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnprocessableEntityException;
+	use Illuminate\Routing\Controller;
+	use Psr\Http\Message\ServerRequestInterface;
+	use Spatie\Fractal\ArraySerializer as ArraySerialization;
+	use Jkirkby91\Boilers\RestServerBoiler\ResourceResponseContract;
+	use Jkirkby91\Boilers\RestServerBoiler\Exceptions\NotFoundHttpException;
+	use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnauthorizedHttpException;
+	use Jkirkby91\Boilers\RestServerBoiler\Exceptions\UnprocessableEntityException;
 
-/**
- * Class RestController
- *
- * A RestController Boiler Plate
- *
- * @package Jkirkby91\LumenRestServerComponent\Http\Controllers
- * @author James Kirkby <jkirkby91@gmail.com>
- */
-abstract class RestController extends Controller
-{
+	/**
+	 * Class RestController
+	 *
+	 * A RestController Boiler Plate
+	 * 
+	 *
+	 * @package Jkirkby91\LumenRestServerComponent\Http\Controllers
+	 * @author  James Kirkby <jkirkby@protonmail.ch>
+	 */
+	abstract class RestController extends Controller
+	{
+		/**
+		 * @var array
+		 */
+		private $privateHeaders = [
+			'Rest-Server'    => 'Jkirkby91\\LumenRestServerComponent',
+			'Version'        => 'Alpha-1.0.1',
+			'content-type'   => 'text/json'
+		]; //@TODO hook headers into our responses
 
-    public $user;    
+		/**
+		 * @var array
+		 */
+		protected $headers = [];
 
-    /**
-     * @var array
-     */
-    private $privateHeaders = [
-        'Rest-Server'    => 'Jkirkby91\\LumenRestServerComponent',
-        'Version'        => 'Alpha-1.0.1',
-        'content-type'   => 'text/json'
-    ]; //@TODO hook headers into our responses
+		/**
+		 * @var \League\Fractal\Serializer\ArraySerializer
+		 */
+		protected $serializer;
 
-    /**
-     * @var array
-     */
-    protected $headers = [];
+		/**
+		 * RestController constructor.
+		 */
+		public function __construct()
+		{
+			// $this->headers->set('Rest-Server',$this->privateHeaders['Rest-Server']);
+			// $this->headers->set('Version',$this->privateHeaders['Version']);
+			// $this->headers->set('content-type',$this->privateHeaders['content-type']);
+			// $this->withHeaders($this->privateHeaders);
+			$this->serializer = new ArraySerialization();
+		}
 
-    /**
-     * @var \League\Fractal\Serializer\ArraySerializer
-     */
-    protected $serializer;
+		/**
+		 * item()
+		 * @param $item
+		 *
+		 * @return $this
+		 */
+		public function item($item)
+		{
+			return fractal()->item($item);
+		}
 
-    /**
-     * RestController constructor.
-     */
-    public function __construct()
-    {
-        // $this->headers->set('Rest-Server',$this->privateHeaders['Rest-Server']);
-        // $this->headers->set('Version',$this->privateHeaders['Version']);
-        // $this->headers->set('content-type',$this->privateHeaders['content-type']);
-        // $this->withHeaders($this->privateHeaders);
-        $this->serializer = new ArraySerialization();
-        $this->user = app()->make('auth')->user();
-    }
+		/**
+		 * collection()
+		 * @param $collection
+		 *
+		 * @return $this
+		 */
+		public function collection($collection)
+		{
+			return fractal()->collection($collection);
+		}
 
-    /**
-     * @param $item
-     * @return $this
-     */
-    public function item($item)
-    {
-        return fractal()->item($item);
-    }
+		/**
+		 * getHeaders()
+		 * @return array
+		 */
+		public function getHeaders()
+		{
+			return $this->headers;
+		}
 
-    /**
-     * @param $collection
-     * @return $this
-     */
-    public function collection($collection)
-    {
-        return fractal()->collection($collection);
-    }
+		/**
+		 * setHeaders()
+		 * @param array $headers
+		 */
+		public function setHeaders(array $headers)
+		{
+			array_push($this->headers, $headers);
+		}
 
-    /**
-     * @return array
-     */
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
+		/**
+		 * paginateResults()
+		 * @param $results
+		 * @param $page
+		 *
+		 * @return mixed
+		 */
+		public function paginateResults($results,$page)
+		{
+			try {
+				$paginatedResults = $this->repository->lumenPaginatedQuery($results,$page);
+			} catch (\Exception $e){
+				throw new UnprocessableEntityException($e->getMessage());
+			}
 
-    public function setHeaders(array $headers)
-    {
-        array_push($this->headers, $headers);
-    }
+			$paginatedResults = $paginatedResults->toArray();
 
-    public function paginateResults($results,$page)
-    {
-       try {
-            $paginatedResults = $this->repository->lumenPaginatedQuery($results,$page);
-        } catch (\Exception $e){
-            throw new UnprocessableEntityException($e->getMessage());
-        }
+			if(isset($paginatedResults))
+			{
+				$paginatedResults['data'] = $this->collection($paginatedResults['data'])
+					->transformWith($this->transformer)
+					->serializeWith(new ArraySerialization());
+				return $paginatedResults;
+			} else {
+				throw new NotFoundHttpException;
+			}
+		}
 
-        $paginatedResults = $paginatedResults->toArray();
+		/**
+		 * getPaginationPageFromRequest()
+		 * @param \Psr\Http\Message\ServerRequestInterface $request
+		 *
+		 * @return int|mixed
+		 */
+		public function getPaginationPageFromRequest(ServerRequestInterface $request)
+		{
+			$queryParams = $request->getQueryParams();
 
-        if(isset($paginatedResults))
-        {
-            $paginatedResults['data'] = $this->collection($paginatedResults['data'])
-                ->transformWith($this->transformer)
-                ->serializeWith(new ArraySerialization());
-            return $paginatedResults;
-        } else {
-            throw new NotFoundHttpException;
-        }         
-    }
+			if(!isset($queryParams['page']) || is_null($queryParams['page'])) {
+				$page = 1;
+			} else {
+				$page = filter_var($queryParams['page'],FILTER_SANITIZE_STRING);
+			}
 
-    public function getPaginationPageFromRequest(ServerRequestInterface $request)
-    {
-        $queryParams = $request->getQueryParams();
-
-        if(!isset($queryParams['page']) || is_null($queryParams['page'])) {
-            $page = 1;
-        } else {
-            $page = filter_var($queryParams['page'],FILTER_SANITIZE_STRING);
-        }  
-
-        return $page;
-    }
-
-    public function validateRequestedUserContentPath($uid)
-    {
-        // return dd($this->user->getId());
-        if($uid != $this->user->getId())
-        {
-            throw new UnauthorizedHttpException;
-        }    
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getRequestUser()
-    {
-        return $this->user;
-    }    
-}
+			return $page;
+		}
+		
+	}
